3d0e10182889c1eb9bea15b71447c3d9863fec24
Nasipa <1abdyraiymova.n@gmail.com>
2025-06-01 20:26:55 +0600

    controller fixes

--- src/main/java/com/nasipa/tictactoewebsocket/controller/WebController.java
+++ src/main/java/com/nasipa/tictactoewebsocket/controller/WebController.java
@@ -1,10 +1,35 @@
 package com.nasipa.tictactoewebsocket.controller;
 
+import com.nasipa.tictactoewebsocket.model.dto.AuthResponseDto;
+import com.nasipa.tictactoewebsocket.model.dto.UserRegistrationDto;
+import com.nasipa.tictactoewebsocket.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class WebController {
+    
+    private final UserService userService;
+    
+    @Autowired
+    public WebController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
 
     @GetMapping("/auth/login")
     public String login() {
@@ -14,5 +39,17 @@ public class WebController {
     @GetMapping("/auth/register")
     public String register() {
         return "register";
+    }
+    
+    @PostMapping("/auth/register")
+    public String processRegistration(@ModelAttribute UserRegistrationDto registrationDto) {
+        AuthResponseDto response = userService.registerUser(registrationDto);
+        
+        if (response.isSuccess()) {
+            return "redirect:/";  // Redirect to the menu/index page
+        } else {
+            // Registration failed
+            return "redirect:/auth/register?error=" + response.getMessage();
+        }
     }
 } 
